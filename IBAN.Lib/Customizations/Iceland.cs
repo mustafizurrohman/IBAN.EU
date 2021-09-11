2affec5575a258f4b84815dca2000a20e051e859
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2021-09-11 21:15:53 +0200

    Added customization for Iceland

---
+++ IBAN.Lib/Customizations/Iceland.cs
@@ -0,0 +1,91 @@
+﻿// ***********************************************************************
+// Assembly         : IBANEU.Lib
+// Author           : Mustafizur Rohman
+// Created          : 09-11-2021
+//
+// Last Modified By : Mustafizur Rohman
+// Last Modified On : 09-11-2021
+// ***********************************************************************
+// <copyright file="Iceland.cs" company="IBANEU.Lib">
+//     Copyright (c) Personal. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using IBANEU.Lib.Core;
+using IBANEU.Lib.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+
+namespace IBANEU.Lib.Customizations
+{
+
+    /// <summary>
+    /// Class Iceland.
+    /// Implements the <see cref="IBANEU.Lib.Customizations.CustomizationBase" />
+    /// </summary>
+    /// <seealso cref="IBANEU.Lib.Customizations.CustomizationBase" />
+    internal class Iceland : CustomizationBase
+    {
+
+        /// <summary>
+        /// Gets or sets the length of the iban.
+        /// </summary>
+        /// <value>The length of the iban.</value>
+        protected override int IBANLength => 26;
+        
+        /// <summary>
+        /// Gets the country code.
+        /// </summary>
+        /// <value>The country code.</value>
+        public override string CountryCode => "IS";
+        
+        /// <summary>
+        /// Parses the iban from string.
+        /// </summary>
+        /// <param name="ibanAsString">The iban as string.</param>
+        /// <returns>IBANEU.Lib.Core.IBANDto.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal override IBANDto ParseIbanFromString(string ibanAsString)
+        {
+            ibanAsString = ibanAsString.RemoveSpaces();
+
+            if (ibanAsString.Length != IBANLength)
+                throw new Exception($"Icelandic IBANs must have {IBANLength} characters");
+
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            IBANDto ibanDto = new IBANDto();
+
+            ibanDto.BankCode = ibanAsString.Substring(4, 4);
+
+            if (!ibanDto.BankCode.ContainsOnlyNumbers())
+                throw new Exception("Icelandic Bank Codes may contain only numbers.");
+
+            ibanDto.BranchCode = ibanAsString.Substring(8, 2);
+
+            if (!ibanDto.BranchCode.ContainsOnlyNumbers())
+                throw new Exception("Icelandic Branch Codes may contain only numbers.");
+            
+            ibanDto.AccountNumber = ibanAsString.Substring(10, 6);
+            
+            if (!ibanDto.AccountNumber.ContainsOnlyNumbers())
+                throw new Exception("Icelandic Account Numbers may contain only numbers.");
+            
+            var checkSum = ibanAsString.Substring(2, 2);
+            var identificationNumber = ibanAsString.Substring(16, 10);
+
+            if (!identificationNumber.ContainsOnlyNumbers())
+                throw new Exception("Icelandic IBAN Identification Numbers may contain only numbers.");
+
+            ibanDto.AsString = ibanAsString;
+
+            ibanDto.AsStringWithSpaces = FormatIBANString(new List<string>
+            {
+                checkSum, ibanDto.BankCode, ibanDto.BranchCode, ibanDto.AccountNumber, identificationNumber
+            });
+
+            return ibanDto;
+
+        }
+    }
+}
